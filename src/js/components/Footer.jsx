6f77231dafc3478460b420f1ec4a194c06494e4d
Pedro Isaac Aguilar <vandeer.fler@gmail.com>
2025-02-18 21:24:47 +0000

    Landing page finalizada

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer=() => {
+
+return(
+    <div className="text-center p-4 bg-dark mt-5" style={{backgroundColor:" rgba(0, 0, 0, 0.05)"}}>
+    <p className="text-white fw-bold">© 2025 Copyright</p>
+  </div>
+)
+
+}
+export default Footer
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
+import Footer from "./Footer";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<div className="container text-center mt-5">
+				<div className="row">
+					<div className="col">
+						<Card
+						imagen={"https://adselams.com/wp-content/uploads/2024/09/graphic-design-page.webp"}
+						/>
+					</div>
+					<div className="col">
+						<Card 
+						imagen={"https://www.sinclair.edu/www/assets/Image/Hom-Aca-AcaDiv-LCS-Aap%26Des/Design.jpg"}
+						/>
+					</div>
+					<div className="col">
+						<Card 
+						imagen={"https://www.udacity.com/_next/image?url=https%3A%2F%2Fvideo.udacity-data.com%2Ftopher%2F2024%2FOctober%2F67098188_cd0027%2Fcd0027.jpg&w=3840&q=75"}
+						/>
+					</div>
+					<div className="col">
+						<Card 
+						imagen={"https://shakuro.com/_next/image?url=https%3A%2F%2Fblog.cgify.com%2Fwp-content%2Fuploads%2F2020%2F01%2Fmeaning-of-design.jpg&w=2560&q=98"}
+						/>
+					</div>
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 };
